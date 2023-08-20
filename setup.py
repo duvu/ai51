2d2ee896eb80a3dbabae2c3e3c7fba09b759114c
duvu <hoaivubk@gmail.com>
2023-08-20 16:43:26 +0700

    Updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ai51',
+    version='0.1.0',
+    description='A package for managing AI',
+    author='x51',
+    author_email='myx51fc@gmail.com',
+    url='https://github.com/duvu/ai51',
+    packages=['ai51'],
+    package_dir={'ai51': 'ai'},
+)
